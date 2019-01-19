3dacf2d7959e1fdb14722a5211b4769fc8473045
Hugh Wang <18501667737@163.com>
2019-01-19 13:10:10 +0800

    by bash

---
+++ hugh-learning-2019011601/src/utils/TextUtil.js
@@ -0,0 +1,14 @@
+
+class TextUtil {
+
+    static isEmpty(text){
+        if(text === undefined
+            || text === null
+            || text === ''){
+            return true;
+        }
+        return false;
+    }
+}
+
+export default TextUtil;
