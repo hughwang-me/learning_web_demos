ba348e6f126dde18266c5d16fefb6afab2f9c013
Hugh Wang <18501667737@163.com>
2019-01-23 08:30:32 +0800

    by bash

---
+++ hugh-learning-react-react-redux/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+npm install --save-dev webpack
+npm install --save-dev webpack-cli
+npm install --save-dev webpack-dev-server
+
+npm install --save-dev babel-loader @babel/core
+npm install --save-dev @babel/preset-react
+
+npm install --save-dev babel-plugin-import
+npm install --save-dev clean-webpack-plugin
+npm install --save-dev html-webpack-plugin
+npm install --save-dev style-loader
+npm install --save-dev css-loader
+
+npm install --save react
+npm install --save react-dom
+npm install --save redux
+npm install --save react-redux
+npm install --save redux-saga
+npm install --save antd
+npm install --save axios
+
+
+
