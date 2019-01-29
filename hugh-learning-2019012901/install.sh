9bfa2279308a8fabb467ca7206fdfcdafcc041ad
Hugh Wang <18501667737@163.com>
2019-01-29 11:41:26 +0800

    by bash

---
+++ hugh-learning-2019012901/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+npm install --save-dev webpack
+npm install --save-dev webpack-cli
+npm install --save-dev webpack-dev-server
+
+npm install react
+npm install react-dom
+npm install --save-dev babel-loader @babel/core
+npm install --save-dev @babel/preset-react
+
+npm install --save-dev clean-webpack-plugin
+npm install --save-dev html-webpack-plugin
+
+npm install --save-dev style-loader
+npm install --save-dev css-loader
+
---
+++ hugh-learning-2019012901/src/commons/OssSettings.js
@@ -0,0 +1,3 @@
+const BASE_URL = 'https://wwww.baidu.com';
+
+export { BASE_URL }
